c0c832727aefd3ad6f8cc3f2519449987edee01d
ds4045 <ds4045@mail.ru>
2023-04-16 17:48:06 +0300

    added call_me button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ConfigProvider, theme } from 'antd';
+import { ConfigProvider, FloatButton, theme } from 'antd';
 import Header from './components/header/Header';
 import ruRU from 'antd/lib/locale/ru_RU';
 import enUS from 'antd/lib/locale/en_US';
@@ -8,7 +8,7 @@ import messages from './translations/messages';
 import Carousel from './components/carousel/Carousel';
 import CryptoTicker from './components/crypto_ticker/CryptoTicker';
 import Popular from './components/popular/Popular';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Register from './components/auth/Register';
 import { useAutoSignIn } from './hooks/useAutoSignIn';
 import Login from './components/auth/Login';
@@ -20,29 +20,23 @@ import NotFound from './components/not_found/NotFound';
 import Cart from './components/cart/Cart';
 import { loadCartFromLocalStorage } from './helpers/localStorage';
 import { pushAddedItems } from './redux/cartSlice';
-import { fetchUsers } from './api/fetchUsers';
 import { fetchItems } from './api/fetchItems';
 import { CategoryType } from './types/types';
 import OrderSuccess from './components/cart/OrderSuccess';
 import AboutUs from './components/about_us/aboutUs';
+import { PhoneOutlined } from '@ant-design/icons';
 
-function App() {
+const App = () => {
   useAutoSignIn();
+  const location = useLocation();
   const dispatch = useAppDispatch();
-  const isAuth = useAppSelector((state) => state.auth.isAuth);
   const me = useAppSelector((state) => state.auth.login);
-
   const [currentCategory, setCurrentCategory] = useState<CategoryType>('Asic');
   const [locale, setLocale] = useState<'ru' | 'en'>('ru');
   const [darkThemes, setDarkThemes] = useState<boolean>(false);
-
   const handleLocaleChange = () => {
     locale === 'en' ? setLocale('ru') : setLocale('en');
   };
-  useEffect(() => {
-    if (isAuth) fetchUsers(dispatch);
-  }, [dispatch, isAuth]);
-
   useEffect(() => {
     dispatch(pushAddedItems(loadCartFromLocalStorage('cart')));
     fetchItems(dispatch);
@@ -94,11 +88,22 @@ function App() {
               <Route path="about-us" element={<AboutUs />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
+            {(location.pathname === '/' ||
+              location.pathname === '/catalog' ||
+              location.pathname === '/cart') && (
+              <FloatButton
+                className="btn_call_me"
+                shape="circle"
+                type="primary"
+                style={{ right: 30 }}
+                icon={<PhoneOutlined />}
+              />
+            )}
           </div>
         </ConfigProvider>
       </IntlProvider>
     </ConfigProvider>
   );
-}
+};
 
 export default App;
